61c4b5375aab722889fc9d879f0c1acf8146f331
Tomo Oga <oga.t@northeastern.edu>
2024-11-14 18:46:10 -0500

    add setup.py and build

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convlstm-mlx',
+    version='0.0.1',
+    description='An MLX implementation of ConvLSTM',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Tomohiro Oga',
+    author_email='oga.t@northeastern.edu',
+    url='https://github.com/tomo-oga/convlstm-mlx',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires='>=3.7'
+)
